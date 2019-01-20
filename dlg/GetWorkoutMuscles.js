c2c4575858df730b36541819168e08f52d96d461
nicolas <nicolas.matteazzi@gmail.com>
2019-01-20 18:52:49 +0100

    Get the muscle based on the type of exercise

--- dlg/GetWorkoutMuscles.js
+++ dlg/GetWorkoutMuscles.js
@@ -24,28 +24,44 @@ exports.do = function(req) {
       if (exercisesResp == null || exercisesResp.exercises == null) {success({muscles: []}); return;}
 
       // The muscles impacted by the workout
-      let muscles = [];
+      var muscles = [];
 
       // The muscles that have been added as a concatenation of 'muscle;muscle;...'
-      let addedMuscles = '';
+      var addedMuscles = '';
 
-      let exercises = exercisesResp.exercises;
+      var exercises = exercisesResp.exercises;
+
+      // Function to add a muscle to the list of impacted muscles
+      var addMuscle = function(m) {
+
+        if (m == null) return;
+
+        // If the muscle hasn't been added, add it
+        if (addedMuscles.indexOf(m) == -1) {
+
+          // Add to the array
+          muscles.push(m);
+
+          // Add to the "added list"
+          addedMuscles += ';' + m;
+        }
+      }
 
       // Iterate to find the muscles
       for (var i = 0; i < exercises.length; i++) {
 
-        // If old data, the muscle group is not there
-        if (exercises[i].muscleGroupId == null) continue;
+        let ex = exercises[i];
 
-        // If the muscle hasn't been added, add it
-        if (addedMuscles.indexOf(exercises[i].muscleGroupId) != -1) {
-
-          // Add to the array
-          muscles.push(exercises[i].muscleGroupId);
-
-          // Add to the "added list"
-          addedMuscles += ';' + exercises[i].muscleGroupId;
+        // Based on the type of exercise, get the muscle
+        if (ex.type == 'single') addMuscle(ex.muscleGroupId)
+        else if (ex.type == 'superset') {
+          addMuscle(ex.ex1.muscleGroupId);
+          addMuscle(ex.ex2.muscleGroupId);
         }
+        else if (ex.type == 'dropset') addMuscle(ex.muscleGroupId);
+        else if (ex.type == 'striping') addMuscle(ex.muscleGroupId);
+        else if (ex.type == 'hourglass') addMuscle(ex.muscleGroupId);
+
       }
 
       // Return the list of muscles
